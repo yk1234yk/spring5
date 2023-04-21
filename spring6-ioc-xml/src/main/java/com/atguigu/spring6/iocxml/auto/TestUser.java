f3e871a4276668c1ea70c87f0af7c2cdf83fbf02
yangke <2215959080@qq.com>
2023-04-21 10:57:00 +0800

    spring6

---
+++ spring6-ioc-xml/src/main/java/com/atguigu/spring6/iocxml/auto/TestUser.java
@@ -0,0 +1,12 @@
+package com.atguigu.spring6.iocxml.auto;
+
+import com.atguigu.spring6.iocxml.auto.controller.UserController;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestUser {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("bean-auto.xml");
+        UserController userController = context.getBean("userController", UserController.class);
+        userController.addUser();
+    }
+}
---
+++ spring6-ioc-xml/src/main/java/com/atguigu/spring6/iocxml/factorybean/TestUser.java
@@ -0,0 +1,11 @@
+package com.atguigu.spring6.iocxml.factorybean;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestUser {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("bean-factorybean.xml");
+        User user =(User) context.getBean("user");
+        System.out.println(user);
+    }
+}
