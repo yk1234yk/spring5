da53cb6d02afdd31cd8efb2b2067f395fda0aca3
yangke <2215959080@qq.com>
2023-05-15 16:17:59 +0800

    spring6

---
+++ spring6-aop/src/main/java/com/atguigu/spring6/aop/xmlaop/CalculatorImpl.java
@@ -0,0 +1,36 @@
+package com.atguigu.spring6.aop.xmlaop;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CalculatorImpl implements Calculator {
+    @Override
+    public int add(int i, int j) {
+        int result = i + j;
+        System.out.println("方法内部result = " + result);
+        return result;
+    }
+
+    @Override
+    public int sub(int i, int j) {
+        int result = i - j;
+        System.out.println("方法内部result = " + result);
+        //模拟异常
+//        int a=1/0;
+        return result;
+    }
+
+    @Override
+    public int mul(int i, int j) {
+        int result = i * j;
+        System.out.println("方法内部result = " + result);
+        return result;
+    }
+
+    @Override
+    public int div(int i, int j) {
+        int result = i / j;
+        System.out.println("方法内部result = " + result);
+        return result;
+    }
+}
